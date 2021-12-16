4fad01f362d6d4dfe2d3ccddeaee10523c6641fd
Mohitur <mohitur669@gmail.com>
2021-12-16 23:45:53 +0530

    test

---
+++ Day 06/functions.py
@@ -0,0 +1,17 @@
+# python functions 
+
+
+# print("Hello")
+# num_char = len("Hello")
+# print(num_char)
+
+
+# user defined function
+
+# function definition
+def my_function():
+    print("Hello")
+    print("Bye")
+
+# function call
+my_function()
---
+++ Day 06/hurdles_loop.py
@@ -0,0 +1,30 @@
+# go to this link to get the map https://bit.ly/3oQmOox
+# Reeborg's World
+
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def one_piece():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+   
+one_piece()
+turn_left()
+one_piece()
+turn_left()
+one_piece()
+turn_left()
+one_piece()
+turn_left()
+one_piece()
+turn_left()
+one_piece()
+turn_left()
